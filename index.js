4017aa5209fef7adced0a75147c895018b4151b3
Sazumi Viki <75471215+SazumiVicky@users.noreply.github.com>
2023-09-23 02:36:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const CharacterAI = require("node_characterai");
+const { AsyncQueue } = require("async-queue");
 
 const app = express();
 
 app.use(express.json());
+
+const queue = new AsyncQueue();
 
 async function initializeBrowser() {
   const browser = await puppeteer.launch({
@@ -30,35 +33,39 @@ app.get("/", async (req, res) => {
       throw new Error("Missing required parameters");
     }
 
-    const isolatedCharacterAI = new CharacterAI();
-    await isolatedCharacterAI.authenticateWithToken(accessToken);
-    
-    const chat = await isolatedCharacterAI.createOrContinueChat(characterId);
-    const start = Date.now();
+    const result = await queue.run(async () => {
+      const isolatedCharacterAI = new CharacterAI();
+      await isolatedCharacterAI.authenticateWithToken(accessToken);
 
-    const browser = await initializeBrowser();
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);
-    page.removeAllListeners('request');
-    page.on('request', (request) => {
-      request.continue();
+      const chat = await isolatedCharacterAI.createOrContinueChat(characterId);
+      const start = Date.now();
+
+      const browser = await initializeBrowser();
+      const page = await browser.newPage();
+      await page.setRequestInterception(true);
+      page.removeAllListeners('request');
+      page.on('request', (request) => {
+        request.continue();
+      });
+
+      const response = await chat.sendAndAwaitResponse(message, true);
+
+      const end = Date.now();
+      const elapsedTime = end - start;
+
+      const jsonResponse = {
+        Developer: "Sazumi Viki",
+        Loaded: `${elapsedTime} ms`,
+        Response: response.text,
+      };
+
+      await browser.close();
+
+      return jsonResponse;
     });
 
-    const response = await chat.sendAndAwaitResponse(message, true);
-
-    const end = Date.now();
-    const elapsedTime = end - start;
-
-    const jsonResponse = {
-      Developer: "Sazumi Viki",
-      Loaded: `${elapsedTime} ms`,
-      Response: response.text,
-    };
-
     res.setHeader("Content-Type", "application/json");
-    res.send(JSON.stringify(jsonResponse, null, 2));
-    
-    await browser.close();
+    res.send(JSON.stringify(result, null, 2));
   } catch (error) {
     console.error("Error:", error.message);
     res.status(500).json({ error: error.message || "Internal server error" });
